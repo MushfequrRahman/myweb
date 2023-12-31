0d71b7775d27bf98b4812c38cacd29745edf40d2
Mushfequr Rahman <me.vgithub@gmail.com>
2023-12-31 15:37:18 +0600

    url link change

--- myweb/mywebapp/urls.py
+++ myweb/mywebapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('myweb', views.mywebapp, name='home'),
+    path('', views.mywebapp, name='home'),
     path('about/', views.about, name='about'),
     path('profile/', views.profile, name='profile'),
 ]
